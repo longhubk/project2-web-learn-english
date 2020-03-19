bfd97ce2089c3ac321dcbca3e755593dbd26ed30
Nguyễn Thành Long <longn7284@gmail.com>
2020-03-20 03:32:13 +0700

    second submit

---
+++ js/calendar.js
@@ -0,0 +1,56 @@
+function generateDay(){
+
+  let calendar = document.getElementById("day")
+  let month = document.getElementById("month")
+  let date = new Date()
+  let day_now = date.getDate()
+  let month_now = date.getMonth()
+  let year_now = date.getFullYear()
+  let n = daysInMonth(month_now+1, year_now)
+
+  let day = [n]
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+
+  
+  for(let i = 1; i <= n; i++){
+    day[i] = document.createElement('div')
+    day_content = document.createTextNode(i)
+    day[i].appendChild(day_content)
+    day[i].id = "day-" + i
+    calendar.appendChild(day[i])
+  }
+  month.innerHTML = monthNames[month_now];
+
+  for(let i = 1; i <= n; i++){
+    let id_day = "day-"+i
+    let today = document.getElementById(id_day)
+    let today_number = today.innerHTML;
+    if( day_now == today_number){
+      today.id = "today";
+    }
+  }
+
+
+}
+function checkTime(t){
+  if(t < 10) t =  "0" + t;
+  return t
+}
+function generateTime(){
+  let date = new Date()
+  let time = document.getElementById("time")
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  let seconds = date.getSeconds()
+  hours = checkTime(hours)
+  minutes = checkTime(minutes)
+  seconds = checkTime(seconds)
+  time.innerHTML = hours + ":" + minutes + ":" + seconds;
+  var t = setTimeout(generateTime, 500);
+}
+function daysInMonth (month, year) { 
+   return new Date(year, month, 0).getDate(); 
+} 
